8f6ced106f18f50742ab795ec846f988d50d8b5d
M40 <modev.404@gmail.com>
2024-12-11 19:29:25 +0300

    connect db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,14 @@
-import mongoose from "mongoose";
-import { dbName, dbUrl } from "./config.js";
 import chalk from "chalk";
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(dbUrl, { dbName });
-    console.log(`${chalk.green.bold("Connected")} to the database`);
-  } catch (error) {
-    console.log(`${chalk.red.bold("Error")} connecting to database`, error);
-    process.exit(1);
-  }
+import { connect } from "mongoose";
+import { dbUrl } from "./config.js";
+export const connectDB = async () => {
+  connect(dbUrl)
+    .then(() => {
+      console.log(`${chalk.green.bold("Connected")} to the database`);
+    })
+    .catch((error) => {
+      console.log(`${chalk.red.bold("Error")} connecting to database`, error);
+      process.exit(1);
+    });
 };
-
 export default connectDB;

36db2a8533bc44949384b578763794293ccf8b19
M40 <modev.404@gmail.com>
2024-12-15 09:46:10 +0300

    post blog

--- frontend/src/store/blogSlice.js
+++ frontend/src/store/blogSlice.js
@@ -1,18 +1,34 @@
 import axiosInstance from "@/lib/axiosUtil";
+import toast from "react-hot-toast";
 import { create } from "zustand";
 
 const useBlogs = create((set) => ({
   blogs: [],
   error: false,
   post: {},
+  loading: false,
   reFetch: function () {
     set({ error: false });
   },
-  fetchBlogs: async function () {
-    const { data } = await axiosInstance.get("/posts/").catch((err) => {
+  fetchBlogs: async () => {
+    const { data } = await axiosInstance.get("/posts/").catch(() => {
       set({ error: true });
     });
     set({ error: false, blogs: data });
+  },
+  postBlog: async (post) => {
+    set({ loading: true });
+    await axiosInstance
+      .post("/posts/create", post)
+      .then(() => {
+        window.location.pathname = "/";
+      })
+      .catch(() => {
+        toast.error("try again..");
+      })
+      .finally(() => {
+        set({ loading: false });
+      });
   },
 }));
 

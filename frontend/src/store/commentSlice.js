6c8323cc410b118b305d181df260be5ad4572820
M40 <modev.404@gmail.com>
2024-12-16 09:04:21 +0300

    create post

--- frontend/src/store/commentSlice.js
+++ frontend/src/store/commentSlice.js
@@ -3,9 +3,21 @@ import { create } from "zustand";
 
 const useComments = create((set, get) => ({
   comments: [],
+  loading: false,
   fetchComments: async (post_id) => {
     const { data } = await axiosInstance.get(`/comments/${post_id}`);
     set({ comments: data });
+  },
+  createComment: async (data) => {
+    set({ loading: true });
+    await axiosInstance
+      .post("/comments/create", data)
+      .catch(() => {
+        toast.error("try again latter");
+      })
+      .finally(() => {
+        set({ loading: false });
+      });
   },
 }));
 

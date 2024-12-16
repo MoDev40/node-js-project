21c7c3f1c0fb6702a75b5c2169665fc525e9a584
M40 <modev.404@gmail.com>
2024-12-16 08:51:59 +0300

    updated

--- backend/src/routes/commentRoute.js
+++ backend/src/routes/commentRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createComment, getCommentsByPostId } from "../controllers/commentController.js";
+import {
+  createComment,
+  getCommentsByPostId,
+} from "../controllers/commentController.js";
 import auth from "../middlewares/middleware.js";
 
 const router = express.Router();
 
 router.post("/create", auth, createComment);
-router.post("/:id",getCommentsByPostId);
+router.get("/:id", getCommentsByPostId);
 export default router;
